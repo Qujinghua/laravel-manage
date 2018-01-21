a35d23beca7654cd433576e66b8b78e5c2ac56d0
Qu Jinghua <893510847@qq.com>
2018-01-21 20:57:09 +0800

    中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'checklogin' => \App\Http\Middleware\Checklogin::class,
+        'checklogin' => \App\Http\Middleware\CheckLogin::class,
     ];
 }
